8302d6d8fbf6e1d403f0786e54408d3711bdea63
Sweetheart502 <869780307@qq.com>
2015-11-30 20:25:14 +0800

    大小端

---
+++ Endian.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+ **	    >  Name : 判断字节序.c
+ **	    > Author: LiYingXiao (Sweethreart502) 
+ **	    >  Mail : liyingxiao502@gmail.com
+ **	    >  Blog : http://blog.csdn.net/u013166575
+ **	    > Created Time: 2015年11月26日 星期四 15时04分28秒
+ ************************************************************************/
+
+//#include <stdio.h>
+#include <iostream>
+
+void byteorder ()
+{
+    union {
+        short value ;
+        char union_bytes[sizeof ( short )] ;
+    } test ;
+
+    test.value = 0x0102 ;
+
+    if ( ( test.union_bytes[0] == 1 ) && ( test.union_bytes[1] == 2 ) ) {
+        std::cout <<  "big endian" << std::endl ;
+    } else if ( ( test.union_bytes[0] == 2 ) && ( test.union_bytes[1] == 1 ) ) {
+        std::cout << "little endian" << std::endl ;
+    } else {
+        std::cout << "unknow." << std::endl ;
+    }
+}
+
+/**
+ * litter endian : 高高低低 (主机字节序)
+ *  big   endian : 高低低高 (网络字节序)
+ */
+
+int main ( int argc , char * argv[] )
+{
+    // 调用函数
+    byteorder () ;
+
+    return 0 ;
+}
+
